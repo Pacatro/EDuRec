fc9a4caff3415c26518e81d99e508289bd25a31d
= <paco.algar.15@gmail.com>
2025-10-19 19:21:34 +0200

    Adapting scripts

--- scripts/run_evals.sh
+++ scripts/run_evals.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-datasets=('mars' 'itm' 'coursera')
+datasets=('mars' 'itm')
 topks=(5 10 15)
 
 for dataset in "${datasets[@]}"; do
   for topk in "${topks[@]}"; do
     echo "Evaluating $dataset dataset on the Top-$topk recommendations"
     uv run edurec eval -d "$dataset" --top_k "$topk"
-    uv run edurec surprise-eval -d "$dataset" --top_k "$topk"
   done
 done
